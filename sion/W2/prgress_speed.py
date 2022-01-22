922fdf4fbad86a4f4a27d8f7f932c08f2a44c14b
Sion Noh <shtldhs123@ajou.ac.kr>
2022-01-23 01:41:11 +0900

    기능개발 - 수정

--- sion/W2/prgress_speed.py
+++ sion/W2/prgress_speed.py
@@ -3,23 +3,25 @@ from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    days = []
-    n = len(progresses)
-    for i in range(n):
+    days = deque()
+    for i in range(len(progresses)):
         days.append(math.ceil((100-progresses[i])/speeds[i]))
         
     while days:
         first_day = days[0]
+        new_days = deque()
         count = 0
-        for i in range(len(days)):
-            days[i] -= first_day
-            if days[i] <= 0:
+        is_continuous = True
+        for day in days:
+            tmp_day = day - first_day
+            new_days.append(tmp_day)
+            if tmp_day > 0:
+                is_continuous = False
+            elif is_continuous:
                 count += 1
-            else: break
         answer.append(count)
-        
-        days = deque(days)
         for _ in range(count):
-            days.popleft()
+            new_days.popleft()
+        days = new_days
     
     return answer
